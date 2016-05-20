e567f63aa8cd9f264737b63bb256314518c6ce0a
gabriel.kato <gabriel.kato@APDT0089>
2016-05-20 13:03:44 -0300

    Added DirectX Monogame implementation support. Pretty sure this is not the right way to do, but worked just fine.

---
+++ DXImplementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DXImplementation
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Runs the DesktopGL implementation using DirectX Dlls (Sorry!)
+            using (var game = new Engine.Game())
+                game.Run();
+        }
+    }
+#endif
+}
